7e33085ade5a1689e65e1e31a20a9ba4f8b23ec9
Jil <jilmehta52@gmail.com>
2022-10-26 12:53:59 -0400

    Migrations

---
+++ WizLib_DataAccess/Migrations/20221026164920_removeGenreTableFromDb.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WizLib_DataAccess.Migrations
+{
+    public partial class removeGenreTableFromDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Genres");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    GenreId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GenreName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.GenreId);
+                });
+        }
+    }
+}
---
+++ WizLib_Model/Models/Genre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WizLib_Model.Models
+{
+    public class Genre
+    {
+        [Key]
+        public int Genre_Id { get; set; }
+
+        public string GenreName { get; set; }
+
+        //public int DisplayOrder { get; set; }
+    }
+}
